53fc02b3a03974cd33fda092ea7797577ca956a3
ornelabaldini <ornelabaldin@gmail.com>
2024-10-25 15:06:28 -0300

    Aparecen mas zombies y menos reciclables

--- Mob.gd
+++ Mob.gd
@@ -5,7 +5,7 @@ func _ready():
 	$AnimatedSprite.playing = true
 	var mob_types = $AnimatedSprite.frames.get_animation_names()
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
-	$AnimatedSprite.speed_scale = 1.5 
+	$AnimatedSprite.speed_scale = 1 
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
